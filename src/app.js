3942e9f866157ad83ac3d28e389183117db55bd4
Gerardo Hirschhorn <ghirschhorn@outlook.com>
2025-06-20 18:37:36 +0000

    random doms

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ['the', 'my', 'our', 'your'];
+  let adjective = ['fuzzy', 'clean', 'crazy', 'smelly'];
+  let noun = ['mother', 'capibara', 'robot', 'coffee'];
+  let extention = ['com', 'es', 'ar', 'net'];
+
+
+  function generateDom() {
+    let randomPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+    let randomAdjective = adjective[Math.floor(Math.random() * adjective.length)];
+    let randomNoun = noun[Math.floor(Math.random() * noun.length)];
+    let randomExtention = extention[Math.floor(Math.random() * extention.length)];
+    return `${randomPronoun}${randomAdjective}${randomNoun}.${randomExtention}`;
+  }
+
+  console.log(generateDom());
+}
